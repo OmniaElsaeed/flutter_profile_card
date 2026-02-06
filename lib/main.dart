e87c236b9591823233d685f84e5817a3d6472ffb
DELL <DELL@DESKTOP-7IB7V8D>
2026-02-06 03:57:00 +0200

    email icon

--- lib/main.dart
+++ lib/main.dart
@@ -18,19 +18,15 @@ class MyApp extends StatelessWidget {
 class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       body: Stack(
         children: [
-
           Positioned.fill(
             child: Image.asset(
               "images/background.jpg",
               fit: BoxFit.cover,
             ),
           ),
-
-
           Positioned(
             top: 70,
             left: 10,
@@ -39,7 +35,6 @@ class HomePage extends StatelessWidget {
               alignment: Alignment.center,
               child: Container(
                 height: 125,
-                //width: 350,
                 decoration: BoxDecoration(
                   color: Color.fromRGBO(13, 148, 136, 0.8),
                   borderRadius: BorderRadius.only(
@@ -57,8 +52,6 @@ class HomePage extends StatelessWidget {
             child: Align(
               alignment: Alignment.center,
               child: Container(
-                //width: 350,
-                //height: 400,
                 decoration: BoxDecoration(
                   color: Color.fromRGBO(17, 24, 39, 0.3),
                   border: Border.all(
@@ -69,213 +62,201 @@ class HomePage extends StatelessWidget {
                     bottomLeft: Radius.circular(25),
                     bottomRight: Radius.circular(25),
                   ),
-
                 ),
-                child:Column(
-                    mainAxisSize: MainAxisSize.min,
-                    children:[
-                      SizedBox(height: 70),
-                      Container(
-
-                        alignment: Alignment.topCenter,
-                        child: Text("Ahmed Mohamed",
-                          style: TextStyle(
-                            color: Colors.white,
-                            //fontWeight: FontWeight.bold,
-                            fontSize: 27,
-                          ),
+                child: Column(
+                  mainAxisSize: MainAxisSize.min,
+                  children: [
+                    SizedBox(height: 70),
+                    Container(
+                      alignment: Alignment.topCenter,
+                      child: Text(
+                        "Ahmed Mohamed",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 27,
                         ),
                       ),
-                      Text("Bussiness Devlopment Manager",
+                    ),
+                    Text(
+                      "Bussiness Devlopment Manager",
+                      style: TextStyle(
+                        color: Colors.white70,
+                        fontSize: 17,
+                      ),
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(5),
+                      child: Text(
+                        "Advanced Tecnology Company",
                         style: TextStyle(
-                          color: Colors.white70,
-                          fontSize: 17,
-                        ),),
-                      Container(
-                        margin: EdgeInsets.all(5),
-                        child: Text("Advanced Tecnology Company", style: TextStyle(
                           color: Colors.white54,
                           fontSize: 14,
-                        ),),
-                      ),
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        child: Divider(
-                          color: Colors.white.withOpacity(0.25),                       thickness: 1,
-                          indent: 40,
-                          endIndent: 40,
                         ),
                       ),
-                      Column(
-                          children: [
-                            ListTile(
-                              leading: Container(
-                                padding: EdgeInsets.all(8),
-                                decoration: BoxDecoration(
-                                  color: Colors.blue.withOpacity(0.2),
-                                  shape: BoxShape.circle,
-                                ),
-                                child: Icon(
-                                  Icons.phone_outlined,
-                                  color: Colors.blue,
-                                  size: 26,
-                                ),
-                              ),
-                              title: Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Phone",
-                                    style: TextStyle(
-                                      color: Colors.white54,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    "+966 50 123 4567",
-                                    style: TextStyle(
-                                      color: Colors.white70,
-                                      fontWeight: FontWeight.w400,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                ],
-                              ),
-                            ),
-                            ListTile(
-                              leading:Container(
-                                padding: EdgeInsets.all(8),
-                                decoration: BoxDecoration(
-                                    color: Colors.deepPurpleAccent.withOpacity(0.2),
-                                    shape: BoxShape.circle
-                                ),
-                                child:Icon(
-                                  Icons.email_outlined,
-                                  color: Colors.deepPurpleAccent,
-                                  size: 26,
-                                ) ,
-                              ) ,
-                              title:Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Email",
-                                    style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    "ahmed.mohamed@company.com",
-                                    style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ],
-                              ) ,
-
-
-
-                            ),
-                            ListTile(
-                              leading:Container(
-                                padding: EdgeInsets.all(8),
-                                decoration: BoxDecoration(
-                                    color: Colors.green.withOpacity(0.2),
-                                    shape: BoxShape.circle
-                                ),
-                                child:Icon(
-                                  Icons.location_on_outlined,
-                                  color: Colors.green,
-                                  size: 26,
-                                ) ,
-                              ) ,
-                              title:Column(
-                                crossAxisAlignment: CrossAxisAlignment.start,
-                                children: [
-                                  Text(
-                                    "Location",
-                                    style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 14,
-                                    ),
-                                  ),
-                                  Text(
-                                    "Riyadh, Saudi Arabia",
-                                    style: TextStyle(
-                                      color: Colors.white70,
-                                      fontSize: 14,
-                                      fontWeight: FontWeight.w400,
-                                    ),
-                                  ),
-                                ],
-                              ) ,
-
-
-
-                            )
-                          ]
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Divider(
+                        color: Colors.white.withOpacity(0.25),
+                        thickness: 1,
+                        indent: 40,
+                        endIndent: 40,
                       ),
-                      Container(
-                        margin: EdgeInsets.all(10),
-                        child: Divider(
-                          color: Colors.white.withOpacity(0.6),
-                          thickness: 1,
-                          indent: 40,
-                          endIndent: 40,
+                    ),
+                    Column(
+                      children: [
+                        ListTile(
+                          leading: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.blue.withOpacity(0.2),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.phone_outlined,
+                              color: Colors.blue,
+                              size: 26,
+                            ),
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Phone",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                "+966 50 123 4567",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontWeight: FontWeight.w400,
+                                  fontSize: 14,
+                                ),
+                              ),
+                            ],
+                          ),
                         ),
+                        ListTile(
+                          leading: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Color.fromRGBO(192, 132, 252, 0.15),
+                              shape: BoxShape.circle,
+                              border: Border.all(
+                                color: Color.fromRGBO(168, 85, 247, 0.4),
+                                width: 2,
+                              ),
+                            ),
+                            child: Icon(
+                              Icons.email_outlined,
+                              color: Colors.purpleAccent,
+                              size: 26,
+                            ),
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Email",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                "ahmed.mohamed@company.com",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        ),
+                        ListTile(
+                          leading: Container(
+                            padding: EdgeInsets.all(8),
+                            decoration: BoxDecoration(
+                              color: Colors.green.withOpacity(0.2),
+                              shape: BoxShape.circle,
+                            ),
+                            child: Icon(
+                              Icons.location_on_outlined,
+                              color: Colors.green,
+                              size: 26,
+                            ),
+                          ),
+                          title: Column(
+                            crossAxisAlignment: CrossAxisAlignment.start,
+                            children: [
+                              Text(
+                                "Location",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                ),
+                              ),
+                              Text(
+                                "Riyadh, Saudi Arabia",
+                                style: TextStyle(
+                                  color: Colors.white,
+                                  fontSize: 14,
+                                  fontWeight: FontWeight.w400,
+                                ),
+                              ),
+                            ],
+                          ),
+                        )
+                      ],
+                    ),
+                    Container(
+                      margin: EdgeInsets.all(10),
+                      child: Divider(
+                        color: Colors.white.withOpacity(0.6),
+                        thickness: 1,
+                        indent: 40,
+                        endIndent: 40,
                       ),
-
-                      Padding(
-                        padding: const EdgeInsets.only(bottom: 20),
-                        child: Row(
-
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: [
-
-                            Container(
-                              margin: EdgeInsets.all(10),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(bottom: 20),
+                      child: Row(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        children: [
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            child: Image.asset(
+                              "images/linkedin.png",
+                              width: 30,
+                              height: 30,
+                              fit: BoxFit.contain,
+                            ),
+                          ),
+                          SizedBox(width: 5),
+                          Container(
+                            margin: EdgeInsets.all(10),
+                            width: 30,
+                            height: 30,
+                            child: ClipOval(
                               child: Image.asset(
-                                "images/linkedin.png",
-                                width: 30,
-                                height: 30,
+                                "images/web.png",
                                 fit: BoxFit.contain,
                               ),
                             ),
-
-
-                            SizedBox(width: 5,)
-                            ,
-                            Container(
-                              margin: EdgeInsets.all(10),
-                              width: 30,
-                              height: 30,
-                              child: ClipOval(
-                                child: Image.asset(
-                                  "images/web.png",
-                                  fit: BoxFit.contain,
-                                ),
-                              ),
-                            ),
-                            SizedBox(width: 5,)
-
-                          ],
-
-                        ),
+                          ),
+                          SizedBox(width: 5),
+                        ],
                       ),
-
-                    ]
-
+                    ),
+                  ],
                 ),
-
               ),
-
             ),
-
           ),
-
           Positioned(
             top: 145,
             left: 0,
@@ -294,10 +275,7 @@ class HomePage extends StatelessWidget {
               ),
             ),
           ),
-
         ],
-
-
       ),
     );
   }
